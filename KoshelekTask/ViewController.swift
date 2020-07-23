7c6c385ed214692d8cc845d592e7c8072a3ae49b
Vlad Yandola <vladan2000.9@mail.ru>
2020-07-23 15:23:39 +0400

    Initial Commit

---
+++ KoshelekTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KoshelekTask
+//
+//  Created by Const. on 23.07.2020.
+//  Copyright © 2020 Oleginc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
